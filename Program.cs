5badaed2721ff3d17d571f18f689742cc87e3d91
leandrolopes-7 <leandrolopes_7@live.com>
2021-07-08 11:52:15 -0300

    Stopwatch

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                Console.WriteLine(time);
+                time--;
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finisher...");
+            Thread.Sleep(2500);
+            Menu();
+        }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+
+            Start(time);
+        }
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite uma opção de contagem ou para sair: ");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M - Minutos => 1m = 1 minuto");
+            Console.WriteLine("0s - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+            string data = Console.ReadLine().ToLower();
+            char option = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (option == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+    }
+}
